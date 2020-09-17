4e3596f59fc511da98525d6e2a695c09d9556e82
Ajay Malhotra <Ajay.Malhotra1@ibm.com>
2020-09-17 14:24:54 -0700

    added code to debug failures in travis

--- directlinkproviderv2/direct_link_provider_v2_integration_test.go
+++ directlinkproviderv2/direct_link_provider_v2_integration_test.go
@@ -556,6 +556,7 @@ var _ = Describe(`DirectLinkProviderV2`, func() {
 					Expect(err).To(BeNil())
 					Expect(detailedResponse.StatusCode).To(Equal(200))
 
+					fmt.Println("After Update status ---- ", *result.OperationalStatus)
 					// if operational status is "provisioned" then we are done
 					if *result.OperationalStatus == "provisioned" {
 						Expect(*result.ID).To(Equal(os.Getenv("GATEWAY_ID")))
@@ -566,6 +567,7 @@ var _ = Describe(`DirectLinkProviderV2`, func() {
 
 					// not provisioned yet, see if we have reached the timeout value.  If so, exit with failure
 					if timer > 24 { // 2 min timer (24x5sec)
+						fmt.Println("After Update and after timer status ---- ", *result.OperationalStatus)
 						Expect(*result.OperationalStatus).To(Equal("provisioned")) // timed out fail if status is not provisioned
 						break
 					} else {
