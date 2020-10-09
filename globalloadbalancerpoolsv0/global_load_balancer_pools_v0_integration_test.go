e925b357283730e5751dff51379005f5c61dc88c
Kumar Ganesan <kuganes1@in.ibm.com>
2020-10-09 15:05:26 +0530

    glb pool regions and origins result update

--- globalloadbalancerpoolsv0/global_load_balancer_pools_v0_integration_test.go
+++ globalloadbalancerpoolsv0/global_load_balancer_pools_v0_integration_test.go
@@ -166,7 +166,7 @@ var _ = Describe(`GlobalLoadBalancerPoolsV0`, func() {
 					}
 				}
 			})
-			It(`GlobalLoadBalancerPoolTest`, func() {
+			It(`GlobalLoadBalancerPool Test`, func() {
 				// create glb pool
 				option := testService.NewCreateLoadBalancerPoolOptions()
 				option.SetName("glbpooltest-pool1")
