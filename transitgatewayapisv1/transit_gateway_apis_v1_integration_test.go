b8b80f453e073fb518729d9a7a3671ebb4275b3a
Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>
2022-11-11 12:07:50 -0600

    feat: adding unbound GRE related changes
    dependency: none
    Signed-off-by: Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>

--- transitgatewayapisv1/transit_gateway_apis_v1_integration_test.go
+++ transitgatewayapisv1/transit_gateway_apis_v1_integration_test.go
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corp. 2020, 2022.
+ * (C) Copyright IBM Corp. 2020,2022.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,10 +31,10 @@ import (
 	"time"
 
 	"github.com/IBM/go-sdk-core/v5/core"
+	"github.com/IBM/networking-go-sdk/transitgatewayapisv1"
 	"github.com/joho/godotenv"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.ibm.com/ibmcloud/networking-go-sdk/transitgatewayapisv1"
 )
 
 var configLoaded = false
