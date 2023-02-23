8a1ca509a4188046bd05c730038304e70c88a5ec
Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>
2023-02-23 13:55:28 -0600

    fix: minor tweaks
    dependency: none

--- zoneratelimitsv1/zone_rate_limits_v1_integration_test.go
+++ zoneratelimitsv1/zone_rate_limits_v1_integration_test.go
@@ -54,6 +54,7 @@ var _ = Describe(`zoneratelimitsv1`, func() {
 		Crn:            &crn,
 		ZoneIdentifier: &zone_id,
 	}
+	defer GinkgoRecover()
 
 	service, serviceErr := NewZoneRateLimitsV1(globalOptions)
 	if serviceErr != nil {
@@ -64,7 +65,7 @@ var _ = Describe(`zoneratelimitsv1`, func() {
 
 	Describe(`zoneratelimitsv1_test`, func() {
 		Context(`zoneratelimitsv1_test`, func() {
-			defer GinkgoRecover()
+			//	defer GinkgoRecover()
 
 			BeforeEach(func() {
 				shouldSkipTest()
